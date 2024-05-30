88e05b2a83f0771e5baa3788d7b331f66d3fd0f7
angelog5 <160875918+angelog5@users.noreply.github.com>
2024-05-30 21:06:22 +0200

    Add files via upload

---
+++ ToDoList.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAddTodo = () => {
+    if (inputValue.trim()) {
+      setTodos([...todos, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const handleRemoveTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleRemoveTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button onClick={handleAddTodo}>Add</button>
+    </div>
+  );
+}
